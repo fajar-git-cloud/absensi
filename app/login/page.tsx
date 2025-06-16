5451d88627f671f34e08427bd2115a5ab61445ab
fajar-git-cloud <muhfaj06@gmail.com>
2025-06-16 20:59:55 +0700

    perbaikan page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
         />
       </div>
 
-      <div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
+      <div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
 
         <div className="w-full h-100">
 
@@ -37,7 +37,7 @@ export default function LoginPage() {
               <input
                 type="password"
                 placeholder="Enter Password"
-                minLength="6"
+                minLength={6}
                 className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                 required
               />
